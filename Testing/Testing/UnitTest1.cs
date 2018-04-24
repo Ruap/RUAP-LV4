8b11639fc55c1add1e89123d0527e66161af57ae
davoraleksic <daleksic14@gmail.com>
2018-04-24 18:21:59 +0200

    Testing

---
+++ Testing/Testing/UnitTest1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumTests
+{
+    [TestFixture]
+    public class UntitledTestCase
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            driver = new FirefoxDriver();
+            baseURL = "https://www.katalon.com/";
+            verificationErrors = new StringBuilder();
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        [Test]
+        public void TheUntitledTestCaseTest()
+        {
+            driver.Navigate().GoToUrl("https://demo.opencart.com/index.php?route=product/category&path=24&sort=pd.name&order=ASC");
+            driver.FindElement(By.LinkText("Phones & PDAs (3)")).Click();
+            driver.FindElement(By.Id("input-limit")).Click();
+            new SelectElement(driver.FindElement(By.Id("input-limit"))).SelectByText("25");
+            driver.FindElement(By.XPath("//option[@value='https://demo.opencart.com/index.php?route=product/category&path=24&limit=25']")).Click();
+            driver.FindElement(By.Id("input-sort")).Click();
+            new SelectElement(driver.FindElement(By.Id("input-sort"))).SelectByText("Price (High > Low)");
+            driver.FindElement(By.XPath("//option[@value='https://demo.opencart.com/index.php?route=product/category&path=24&sort=p.price&order=DESC&limit=25']")).Click();
+        }
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+
+        private string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alertText;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
+        }
+    }
+}
